6fae111da3e5b7df6a32fd8f38a9fa3ba4131198
Regis David Souza Mesquita <github@rdsm.dev>
2024-10-07 22:33:12 +0100

    Was getting 403s, authscheme must be on the request body

--- lib/apple_auth/token.rb
+++ lib/apple_auth/token.rb
@@ -75,6 +75,18 @@ module AppleAuth
       }
     end
 
+    # Apple seems to expect the auth_scheme to be on
+    # the body, the default is :basic_auth
+    def auth_scheme
+      {
+        auth_scheme: :request_body
+      }
+    end
+
+    def oauth_options
+      client_urls.merge(auth_scheme)
+    end
+
     def reponse_hash(access_token)
       token_hash = { access_token: access_token.token }
 
@@ -91,8 +103,11 @@ module AppleAuth
     def apple_access_token
       client = ::OAuth2::Client.new(APPLE_CONFIG.apple_client_id,
                                     client_secret_from_jwt,
-                                    client_urls)
+                                    oauth_options)
       client.auth_code.get_token(code, { redirect_uri: APPLE_CONFIG.redirect_uri }, {})
+    rescue StandardError => e
+      puts "#{e.message} #{e.backtrace}"
+      raise e
     end
   end
 end
